ea5e6b9908c7d8bae48c90185c35a1138963d65d
windowPC <drivespublic@gmail.com>
2022-04-25 00:05:01 +0900

    motion finished

--- animations/src/App.tsx
+++ animations/src/App.tsx
@@ -1,12 +1,11 @@
 import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: linear-gradient(
@@ -17,72 +16,57 @@ const Wrapper = styled(motion.div)`
 `;
 
 const Box = styled(motion.div)`
-  position: absolute;
   display: flex;
-  justify-content: center;
   align-items: center;
-  font-size: 25px;
-  width: 300px;
-  top: 100px;
-  height: 180px;
+  justify-content: center;
+  height: 250px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 35px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  entry: (back: boolean) => {
-    return {
-      x: back ? -500 : 500,
-      opacity: 0,
-      scale: 0,
-    };
-  },
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.5,
-    },
-  },
-  exit: (back: boolean) => {
-    return {
-      x: back ? 500 : -500,
-      opacity: 0,
-      scale: 0,
-    };
-  },
-};
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-
-  const next = () => {
-    setVisible((prev) => (prev === 10 ? 1 : prev + 1));
-    setBack(false);
-  };
-  const prev = () => {
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-    setBack(true);
-  };
+  const [id, setId] = useState<string | null>(null);
   return (
     <Wrapper>
-      <AnimatePresence exitBeforeEnter custom={back}>
-        <Box
-          custom={back}
-          key={visible}
-          variants={boxVariants}
-          initial="entry"
-          animate="center"
-          exit="exit"
-        >
-          {visible}
-        </Box>
+      <Grid>
+        {["1", "2", "3", "4"].map((n) => (
+          <Box onClick={() => setId(n)} key={n} layoutId={n}></Box>
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            onClick={() => setId(null)}
+            initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+            animate={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+            exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+          >
+            <Box layoutId={id} style={{ width: 400, height: 200 }} />
+          </Overlay>
+        ) : null}
       </AnimatePresence>
-      <button onClick={next}>Next</button>
-      <button onClick={prev}>Prev</button>
     </Wrapper>
   );
 }
